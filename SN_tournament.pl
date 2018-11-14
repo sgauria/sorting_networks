a6300e96ef77344d5270281ba48b06bb3119b3ee
Sameer Gauria <sameergauria@yahoo.com>
2018-11-14 12:15:50 -0800

    Actual graph and much improvements.

--- SN_tournament.pl
+++ SN_tournament.pl
@@ -16,7 +16,7 @@ $num_players //= 8;
 #use Algorithm::Networksort::Best qw(:all);
 # Since it is mess to install these libraries, we just use the web interface to the library instead.
 
-# Get the list of comparisons
+# Get the list of matches
 use LWP::Simple;
 my $webpage = `GET http://jgamble.ripco.net/cgi-bin/nw.cgi?inputs=$num_players&algorithm=best&output=text`;
 if ($webpage =~ m/(\[\[.*\]\])/s) {
@@ -27,15 +27,16 @@ $webpage = "$webpage\n";
 #print $webpage;
 
 # Convert string to an actual perl array;
-my @array_cmps = eval ($webpage);
+my @tournament_of_matches = eval ($webpage);
 
 use Data::Dumper;
 $Data::Dumper::Terse = 1; 
 $Data::Dumper::Indent = 0; 
 $Data::Dumper::Maxdepth = 4;  
-#print "\n" . Dumper(\@array_cmps) . "\n";
+#print "\n" . Dumper(\@tournament_of_matches) . "\n";
 
 my @players = (0 .. ($num_players - 1));
+my $num_rounds = $#tournament_of_matches + 1;
 
 # Write out the graph that we want.
 my $tdot_file = "tournament.dot";
@@ -51,26 +52,59 @@ digraph "tournament" {
 DONE
 
 # Space for player names.
-print TDOT_FILE "  { node [shape=rect width=5]\n";
+print TDOT_FILE "  { node [shape=rect width=6 height=1] rank=same\n";
 foreach $p (@players) {
   print TDOT_FILE "    player_name_${p}\n";
 }
 print TDOT_FILE "  }\n";
 
 # Space for player numbers.
-print TDOT_FILE "  { node [shape=circle]\n";
-foreach $p (@players) {
-  print TDOT_FILE "    player_number_level0_${p}\n";
+foreach $r (0 .. $num_rounds) {
+  print TDOT_FILE "  { node [shape=circle width=1 height=1] rank=same\n";
+  foreach $p (@players) {
+    my $lbl_cmnt = $r == 0 ? "[ label=\"${p}\" ]" : "";
+    print TDOT_FILE "    round${r}_player_number_${p} $lbl_cmnt\n";
+  }
+  print TDOT_FILE "  }\n";
 }
-print TDOT_FILE "  }\n";
 
 # First set of edges
 print TDOT_FILE "  { edge []\n";
 foreach $p (@players) {
-  print TDOT_FILE "    player_name_${p} -> player_number_level0_${p}\n";
+  print TDOT_FILE "    player_name_${p} -> round0_player_number_${p}\n";
 }
 print TDOT_FILE "  }\n";
 
+my $src_round_num = 0;
+foreach $rm (@tournament_of_matches) {
+  my @round_of_matches = @{$rm};
+  my $sl = $src_round_num;
+  my $dl = $sl + 1;
+  print TDOT_FILE "  // Round ${dl}\n";
+  print TDOT_FILE "  { edge []\n";
+
+  my %player_connected = {};
+  foreach $mm (@round_of_matches) {
+    my @match = @{$mm};
+    my ($p1, $p2) = @match; 
+    print TDOT_FILE "    // match is between ${p1} and ${p2}\n";
+    print TDOT_FILE "    round${sl}_player_number_${p1} -> round_${sl}to${dl}_match_${p1} -> round${dl}_player_number_${p1}\n";
+    print TDOT_FILE "    round${sl}_player_number_${p2} -> round_${sl}to${dl}_match_${p2} -> round${dl}_player_number_${p2}\n";
+    print TDOT_FILE "    round_${sl}to${dl}_match_${p1} -> round_${sl}to${dl}_match_${p2}\n\n";
+    $player_connected{$p1} = 1; 
+    $player_connected{$p2} = 1; 
+  }
+  foreach $p (@players) { 
+    if (not defined $player_connected{$p} or not $player_connected{$p}) {
+      print TDOT_FILE "    // ${p} has no match in this round\n";
+      print TDOT_FILE "    round${sl}_player_number_${p} -> round${dl}_player_number_${p}\n\n";
+    } 
+    $player_connected{$p} = 0;
+  }
+  print TDOT_FILE "  }\n";
+
+  $src_round_num++;
+}
 
 
 # graph footer
