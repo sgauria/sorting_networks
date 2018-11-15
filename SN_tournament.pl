9df8415e02efc7a3fa2a17094956bd01b8d02279
Sameer Gauria <sameergauria@yahoo.com>
2018-11-14 23:40:10 -0800

    Attempt at ordering things correctly, but still can't figure out how to get dot to arrange things right

--- SN_tournament.pl
+++ SN_tournament.pl
@@ -39,8 +39,8 @@ my @players = (0 .. ($num_players - 1));
 my $num_rounds = $#tournament_of_matches + 1;
 
 # Write out the graph that we want.
-my $tdot_file = "tournament.dot";
-my $tpng_file = "tournament.png";
+my $tdot_file = "tournament_${num_players}.dot";
+my $tpng_file = "tournament_${num_players}.png";
 open TDOT_FILE, ">$tdot_file";
 
 my $scale_factor = 0.5;
@@ -49,6 +49,7 @@ sub scl { my ($x) = @_; return ($x * $scale_factor); }
 # graph header
 print TDOT_FILE "digraph \"tournament\" {\n";
 print TDOT_FILE "  graph [center=1 rankdir=LR ]\n";
+#print TDOT_FILE "  graph [center=1 rankdir=TB ]\n";
 print TDOT_FILE "  edge [dir=none]\n";
 print TDOT_FILE "  node [width=".&scl(0.3)." height=".&scl(0.3)." label=\"\"]\n";
 
@@ -78,6 +79,7 @@ foreach $p (@players) {
 }
 print TDOT_FILE "  }\n";
 
+# All the rounds
 my $src_round_num = 0;
 foreach $rm (@tournament_of_matches) {
   my @round_of_matches = @{$rm};
@@ -91,22 +93,33 @@ foreach $rm (@tournament_of_matches) {
     my @match = @{$mm};
     my ($p1, $p2) = @match; 
     print TDOT_FILE "    // match is between ${p1} and ${p2}\n";
-    print TDOT_FILE "    round${sl}_player_number_${p1} -> round_${sl}to${dl}_match_${p1} -> round${dl}_player_number_${p1} [weight=10]\n";
-    print TDOT_FILE "    round${sl}_player_number_${p2} -> round_${sl}to${dl}_match_${p2} -> round${dl}_player_number_${p2} [weight=10]\n";
-    print TDOT_FILE "    round_${sl}to${dl}_match_${p1} -> round_${sl}to${dl}_match_${p2} [weight=0]\n\n";
+    print TDOT_FILE "    round${sl}_player_number_${p1} -> round_${sl}to${dl}_match_${p1} -> round${dl}_player_number_${p1} [ weight=10 ]\n";
+    print TDOT_FILE "    round${sl}_player_number_${p2} -> round_${sl}to${dl}_match_${p2} -> round${dl}_player_number_${p2} [ weight=10 ]\n";
+    print TDOT_FILE "    round_${sl}to${dl}_match_${p1} -> round_${sl}to${dl}_match_${p2} [ weight=0 ]\n\n";
     $player_connected{$p1} = 1; 
     $player_connected{$p2} = 1; 
   }
   foreach $p (@players) { 
     if (not defined $player_connected{$p} or not $player_connected{$p}) {
       print TDOT_FILE "    // ${p} has no match in this round\n";
-      print TDOT_FILE "    round${sl}_player_number_${p} -> round${dl}_player_number_${p} [weight=10]\n\n";
+      print TDOT_FILE "    round${sl}_player_number_${p} -> round${dl}_player_number_${p} [ weight=10 ]\n\n";
     } 
     $player_connected{$p} = 0;
   }
   print TDOT_FILE "  }\n";
 
   $src_round_num++;
+}
+
+# Some extra invisible edges to get things to render properly.
+foreach $r (0 .. $num_rounds) {
+  #print TDOT_FILE "  { edge [ ] \n";
+  print TDOT_FILE "  { edge [ style=\"invis\" ] \n";
+  foreach $p (0 .. ($num_players - 2)) {
+    my $q = $p + 1;
+    print TDOT_FILE "    round${r}_player_number_${p} -> round${r}_player_number_${q} [ weight=100 ]\n";
+  }
+  print TDOT_FILE "  }\n";
 }
 
 
